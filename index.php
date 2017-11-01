c90d49becdd42d0f1c0d9bedf62dd89b98fe7bfe
Sucharita Das <sd686@afsconnect2.njit.edu>
2017-10-31 20:30:18 -0400

    connection successful

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+$dsn = 'mysql:dbname=sd686;host=sql1.njit.edu';
+$user = 'sd686';
+$password = 'ZOm1EN5l3';
+try {
+    $dbh = new PDO($dsn, $user, $password);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo 'Connection failed: ' . $e->getMessage().'</br>';
+}
+echo 'Connected successfully</br>';
+?>
