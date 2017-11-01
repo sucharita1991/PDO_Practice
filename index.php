b538e2a2b5f52505a2f716b05d1ab7d95e61248d
Sucharita Das <sd686@afsconnect2.njit.edu>
2017-10-31 20:32:19 -0400

    editting connection

--- index.php
+++ index.php
@@ -5,8 +5,8 @@ $password = 'ZOm1EN5l3';
 try {
     $dbh = new PDO($dsn, $user, $password);
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo 'Connected successfully</br>';
 } catch (PDOException $e) {
     echo 'Connection failed: ' . $e->getMessage().'</br>';
 }
-echo 'Connected successfully</br>';
 ?>
