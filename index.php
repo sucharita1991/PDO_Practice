32d1747656851afa35b1c272a3b8cdbd3cfbeee6
Sucharita Das <sd686@afsconnect2.njit.edu>
2017-11-01 16:30:51 -0400

    table generation

--- index.php
+++ index.php
@@ -3,27 +3,46 @@ $dsn = 'mysql:dbname=sd686;host=sql1.njit.edu';
 $user = 'sd686';
 $password = 'ZOm1EN5l3';
 try {
+    //new connection
     $dbh = new PDO($dsn, $user, $password);
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo 'Connected successfully</br>';
 } catch (PDOException $e) {
+    //connection failure
     echo 'Connection failed: ' . $e->getMessage().'</br>';
 }
 
-//$sql = "Select * from accounts where id<6";
 try {
 
-    //$dbh->exec($sql);
-
+    $htmlTable="";
+    $formTable="";
+    $arraylength=0;
+    //prepared statement
     $statement = $dbh->prepare('SELECT * FROM accounts where ID<6');
     $statement->execute();
-    while($result = $statement->fetch(PDO::FETCH_OBJ)) {
+
+    $htmlTable .='<table border="1">';
+    //table header generation
+    $htmlTable .= '<tr><th>ID</th><th>Email</th><th>Fname</th><th>Lname</th><th>Phone</th><th>Birthday</th><th>Gender</th><th>Password</th></tr>';
+    //table row generation
+    while($result = $statement->fetch(PDO::FETCH_ASSOC)) {
         $results[] = $result;
+        $htmlTable .= '<tr><td>'.$result['id'].'</td><td>'.$result['email'].'</td><td>'.$result['fname'].'</td>';
+        $htmlTable .= '<td>'.$result['lname'].'</td><td>'.$result['phone'].'</td><td>'.$result['birthday'].'</td>';
+        $htmlTable .= '<td>'.$result['gender'].'</td><td>'.$result['password'].'</td></tr>';
     }
-    echo 'Number of records: '.count($results).'</br>';
-    
+
+    $htmlTable .= '</table>';
+
+    //Counting number of records fetched
+    $formTable .= 'Number of records: '.count($results).'</br>';
+    $formTable .= $htmlTable;
+
+    echo $formTable;
 
 } catch (PDOException $e) {
+
+    //sql query failure
     echo $sql . "<br>" . $e->getMessage();
 }
 ?>
